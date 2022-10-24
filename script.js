db1985e7e2ff9ad8f61b454ad956933424e4c4bd
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2022-10-24 20:50:02 +0100

    new change

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 let url_format =
   /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/gim;
 
-
 $(".icon-menu").click(function (e) {
   e.preventDefault();
   $(".nav-list").toggle();
 });
 
 $("#Shorten").click(function (e) {
- let url_input = $("#url-text").val();
-    if (url_format.exec(url_input)) {
-        $(".on-response").show("");
-       $(".response-header").html(url_input);
-       $.ajax({
-        type: "Post",
-        url: "https://api.shrtco.de/v2/shorten/www.facebook.com",
-        data: url_input,
-        dataType: "text",
-        success: function (response) {
-            console.log(response);
-        }
-       });
-        
-    } else {
-        $(".info-message").html("Please add a link");
-        $(".url-text").css({"outline": "2px solid red"});
-    }
+  var url_input = $("#url-text").val();
+  if (url_format.exec(url_input)) {
+    $(".on-response").show("");
+    $(".response-header").html(url_input);
+
+    $.post(
+      "https://api.shrtco.de/v2/shorten/",
+      { url: `${url_input}` },
+      function (data, textStatus) {
+      },
+      "json"
+    );
+  } else {
+    $(".info-message").html("Please add a link");
+    $(".url-text").css({ outline: "2px solid red" });
+  }
+});
+
+$("#url-text").keypress(function (e) { 
+    (function (){
+        $(".url-text").css({ outline: "none" });
+        $(".info-message").html("");
+    }());
+});
 
 
+$(".Copy").click(function (e) {
+  e.preventDefault();
+  navigator.clipboard.writeText(shorten_url);
 });
